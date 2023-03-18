e31dc4c2080f558393d54420b5a7807005ec33fa
irina p <gorohovairina21@gmail.com>
2023-03-18 21:40:58 +0300

    Initial commit

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,63 @@
+package ru.netology.stats;
+
+public class StatsService {
+
+    public long sum(long[] sales) {
+        long sumSales = 0; // сумма всех продаж
+        for (long sale : sales) {
+            sumSales += sale;
+        }
+        return sumSales;
+    }
+
+    public long average(long[] sales) {
+        long sumSales = 0; // средняя сумма продаж в месяц
+        for (long sale : sales) {
+            sumSales += sale;
+        }
+        return sumSales / sales.length;
+    }
+
+    public int maxSales(long[] sales) {
+        int maxMonth = 0; // номер месяца с максимальными продажами среди просмотренных ранее
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= sales[maxMonth]) {
+                maxMonth = i;
+            }
+        }
+        return maxMonth + 1; // месяца нумеруются с 1, а индексы массива с 0, нужно сдвинуть ответ на 1
+    }
+
+    public int minSales(long[] sales) {
+        int minMonth = 0; // номер месяца с минимальными продажами среди просмотренных ранее
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] <= sales[minMonth]) { // значит, в рассматриваемом i-м месяце продаж меньше
+                minMonth = i; // запомним его как минимальный
+            }
+        }
+
+        return minMonth + 1; // месяца нумеруются с 1, а индексы массива с 0, нужно сдвинуть ответ на 1
+    }
+
+    public int underAverage(long[] sales) {
+        int counter = 0;
+        for (long sale : sales) {
+            if (sale < average(sales)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public int aboveAverage(long[] sales) {
+        int counter = 0;
+        for (long sale : sales) {
+            if (sale > average(sales)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
